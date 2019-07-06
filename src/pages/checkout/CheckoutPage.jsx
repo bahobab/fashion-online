a5e72980ce0bae1ba6b7d75fccd4c74bc342a830
mono motapa <bahobab@hotmail.com>
2019-07-06 01:45:33 -0500

    added style to checkoutPage header and cartItems total

--- src/pages/checkout/CheckoutPage.jsx
+++ src/pages/checkout/CheckoutPage.jsx
@@ -1,11 +1,37 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {createStructuredSelector} from 'reselect';
+
+import {selectCartItems, selectCartTotal} from '../../redux/cart/cart.selector';
 
 import './checkout.styles.scss';
 
-const CheckoutPage = props => (
-    <div className="checkout">
-        CHeCKOUT
+const CheckoutPage = ({carItems, total}) => (
+    <div className="checkout-page">
+        <div className="checkout-header">
+            <div className="header-block">
+                <span>Product</span>
+            </div>
+            <div className="header-block">
+                <span>Description</span>
+            </div>
+            <div className="header-block">
+                <span>Quantity</span>
+            </div>
+            <div className="header-block">
+                <span>Price</span>
+            </div>
+            <div className="header-block">
+                <span>Remove</span>
+            </div>
+        </div>
+        {carItems.map(cartItem => cartItem.name)}
+        <div className="total">
+            <span>TOTAL: ${total}</span>
+        </div>
     </div>
 );
 
-export default CheckoutPage;
+const mapStateToProps = createStructuredSelector({carItems: selectCartItems, total: selectCartTotal})
+
+export default connect(mapStateToProps)(CheckoutPage);
--- src/redux/cart/cart.selector.js
+++ src/redux/cart/cart.selector.js
@@ -6,4 +6,6 @@ export const selectCartItems = createSelector([selectCart], (cart) => cart.cartI
 
 export const selectCartItemsCount = createSelector([selectCartItems], cartItems => cartItems.reduce((accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity, 0))
 
-export const selectCartHidden = createSelector([selectCart], cart => cart.hidden)
+export const selectCartHidden = createSelector([selectCart], cart => cart.hidden);
+
+export const selectCartTotal = createSelector([selectCartItems], cartItems => cartItems.reduce((accumulatedTotal, cartItem) => accumulatedTotal + cartItem.quantity * cartItem.price, 0))
