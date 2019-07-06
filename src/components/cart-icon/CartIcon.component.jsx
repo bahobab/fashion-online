f90d3b2d22ae23e81f2ef46497db898ae11731ee
mono motapa <bahobab@hotmail.com>
2019-07-05 22:46:24 -0500

    computed total items in cart using data from cartReducer and Array.reduce()

--- src/components/cart-icon/CartIcon.component.jsx
+++ src/components/cart-icon/CartIcon.component.jsx
@@ -6,18 +6,18 @@ import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg'
 
 import './CartIcon.styles.scss';
 
-const CartIcon = ({toggleCartHidden}) => (
+const CartIcon = ({toggleCartHidden, cartItems}) => (
     <div className="cart-icon" onClick={toggleCartHidden}>
         <ShoppingIcon className="shopping-icon"/>
-        <span className="item-count">0</span>
+        <span className="item-count">{cartItems.reduce((accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity, 0)
+}</span>
     </div>
 );
 
-// const mapStateToProps = state => (     {         hidden: state.cart.hidden  }
-// );
+const mapStateToProps = state => ({cartItems: state.cart.cartItems});
 
 const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
-})
+});
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
