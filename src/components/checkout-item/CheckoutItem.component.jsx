b606ab2b17b9ab3b5c3917ef51d6e9a913cdb629
mono motapa <bahobab@hotmail.com>
2019-07-06 03:06:42 -0500

    Implemented remove item from cart functionality

--- src/components/checkout-item/CheckoutItem.component.jsx
+++ src/components/checkout-item/CheckoutItem.component.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import {connect} from 'react-redux';
+
+import {removeItemFromCart} from '../../redux/cart/cart.actions.js';
 
 import './CheckoutItem.styles.scss'
 
-const CheckOutItem = (props) => {
-    const {name, imageUrl, price, quantity} = props.cartItem;
+const CheckOutItem = ({cartItem, removeItemFromCart}) => {
+    const {name, imageUrl, price, quantity} = cartItem;
     return <div className="checkout-item">
         <div className="image-container">
             <img src={imageUrl} alt="item"/>
@@ -11,8 +14,12 @@ const CheckOutItem = (props) => {
         <span className="name">{name}</span>
         <span className="quantity">{quantity}</span>
         <span className="price">{price}</span>
-        <div className="remove-button">&#10005;</div>
+        <div className="remove-button" onClick={() => removeItemFromCart(cartItem)}>&#10005;</div>
     </div>
 };
 
-export default CheckOutItem;
+const mapDispatchToProps = dispatch => ({
+    removeItemFromCart: item => dispatch(removeItemFromCart(item))
+})
+
+export default connect(null, mapDispatchToProps)(CheckOutItem);
--- src/redux/cart/cart.action.types.js
+++ src/redux/cart/cart.action.types.js
@@ -1,4 +1,5 @@
 export const CartActionTypes = {
     TOGGLE_HIDDEN: 'TOGGLE_HIDDEN',
-    ADD_ITEM: 'ADD_ITEM'
+    ADD_ITEM: 'ADD_ITEM',
+    REMOVE_ITEM_FROM_CART: 'REMOVE_ITEM_FROM_CART'
 };
